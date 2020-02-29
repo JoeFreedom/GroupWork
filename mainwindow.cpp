cad13eba64c0ef8603babdb213555f865627101e
JoeFreedom <39825386+JoeFreedom@users.noreply.github.com>
2020-02-29 20:00:53 +0300

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,28 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_tabWidget_tabCloseRequested(int index)
+{
+    if (index != 0){
+       ui->tabWidget->removeTab(index);
+    }
+}
+
+void MainWindow::on_buttonStudent_clicked()
+{
+    ui->tabWidget->addTab(new Student, "Student");
+    ui->tabWidget->setCurrentIndex(ui->tabWidget->count() -1);
+}
